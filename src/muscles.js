52235e1ef6ad58785155f1f385f5005f67febbb9
Tory Herman <tory.herman12@gmail.com>
2017-04-27 22:02:09 -0500

    add muscles A-B

---
+++ src/muscles.js
@@ -0,0 +1,130 @@
+module.export = {
+  MUSCLES: {
+    'abductor digiti minimi of the foot': {
+      'origin': ['medial and lateral process of posterior calcaneal tuberosity'],
+      'insertion': ['lateral side of base of proximal phalanx of the fifth toe and fifth metatarsal'],
+      'action': ['flexes and abducts fifth toe', 'supports lateral longitudinal arch'],
+      'nerve': ['lateral plantar (S2 3)']
+    },
+    'abductor digiti minimi of the hand': {
+      'origin': ['pisiform bone', 'pisohamate ligament', 'flexor retinaculum'],
+      'insertion': ['ulnar side of base of proximal phalanx of little finger and extensor expansion'],
+      'action': ['abducts little finger at metacarpophalangeal joint'],
+      'nerve': ['deep branch of ulnar nerve (C8 T1)']
+    },
+    'abductor hallucis': {
+      'origin': ['medial process of posterior calcaneal tuberosity', 'flexor retinaculum'],
+      'insertion': ['medial aspect of base of proximal phalanx of big toe via medial sesamoid'],
+      'action': ['flexes and abducts big toe', 'supports medial longitudinal arch'],
+      'nerve': ['medial plantar nerve (S1 2)']
+    },
+    'abductor pollicis brevis': {
+      'origin': ['tubercle of scaphoid', 'flexor retinaculum'],
+      'insertion': ['radial sesamoid of proximal phalanx of thumb', 'tendor of extensor pollicis longus'],
+      'action': ['abducts thumb at metacarpophalangeal and carpometacarpal joints'],
+      'nerve': ['recurrent branch of median nerve (C8 T1)']
+    },
+    'abductor pollicis longus': {
+      'origin': ['upper posterior surface of ulna', 'middle third of posterior surface of radius', 'interosseous membrane'],
+      'insertion': ['over tendons of radial extensors and brachioradialis to base of first metacarpal and trapezium'],
+      'action': ['abducts and extends thumb at carpometacarpal joint'],
+      'nerve': ['posterior interosseous nerve (C6 7)']
+    },
+    'adductor brevis': {
+      'origin': ['inferior ramus of body of pubis'],
+      'insertion': ['upper third of linea aspera'],
+      'action': ['adducts hip'],
+      'nerve': ['anterior division of obturator nerve (L2 3)']
+    },
+    'adductor hallucis': {
+      'origin': ['oblique head: base of 2 3 and 4 metatarsals', 'transverse head: plantar metatarsal ligaments and deep transverse ligament'],
+      'insertion': ['lateral side of base of proximal phalanx of big toe', 'lateral sesamoid'],
+      'action': ['adducts and flexes metatarsophalangeal joint of big toe', 'supports transverse arch'],
+      'nerve': ['deep branch of lateral plantar nerve (S2 3)']
+    },
+    'adductor longus': {
+      'origin': ['body of pubis inferior and medial to pubic tubercle'],
+      'insertion': ['lower two thirds of medial linea aspera'],
+      'action': ['adducts and medially rotates hip'],
+      'nerve': ['anterior division of obturator nerve (L2 3)']
+    },
+    'adductor magnus': {
+      'origin': ['adductor portion: ischiopubic ramus', 'hamstring portion: lower outer quadrant of posterior surface of ischial tuberosity'],
+      'insertion': ['adductor portion: lower gluteal line and linea aspera', 'hamstring portion: adductor tubercle'],
+      'action': ['adductor portion: adducts and medially rotates hip', 'hamstring portion: extends hip'],
+      'nerve': ['adductor portion: posterior division of obturator nerve (L2 through 4)', 'hamstring portion: tibial portion of sciatic (L4 through S3)']
+    },
+    'adductor pollicis': {
+      'origin': ['oblique head: base of second and third metacarpals, trapezoid, and capitate', 'transverse head: palmar border and shaft of third metacarpal'],
+      'insertion': ['ulnar sesamoid', 'ulnar side of base of proximal phalanx', 'tendon of extensor pollicis longus'],
+      'action': ['adducts carpometacarpal joint of thumb'],
+      'nerve': ['deep branch of ulnar nerve (T1)']
+    },
+    'anconeus': {
+      'origin': ['smooth surface of lower extremity of posterior aspect of lateral epicondyle of humerus'],
+      'insertion': ['lateral side of olecranon'],
+      'action': ['weakly extends elbow', 'abducts ulna in pronation'],
+      'nerve': ['radial nerve (C7 8)']
+    },
+    'articularis cubiti': {
+      'origin': ['deep distal surface of medial head of triceps'],
+      'insertion': ['posterior capsule of elbow joint'],
+      'action': ['lifts capsule away from joint'],
+      'nerve': ['radial nerve (C6 7 8)']
+    },
+    'articularis genu': {
+      'origin': ['two slips from anterior femur below vastus intermedialius'],
+      'insertion': ['apex of suprastellar bursa'],
+      'action': ['retracts bursa as knee extends'],
+      'nerve': ['posterior division of femoral nerve (L3 4)']
+    },
+    'aryepiglotticus': {
+      'origin': ['apex of arytenoid cartilage'],
+      'insertion': ['lateral border of epiglottis'],
+      'action': ['aids closure of additus of larynx'],
+      'nerve': ['recurrent laryngeal branch of vagus nerve (X)']
+    },
+    'auricularis': {
+      'origin': ['cartilage of auricle'],
+      'insertion': ['galeal aponeurosis'],
+      'action': ['adjusts position of ear'],
+      'nerve': ['temporal branch of facial nerve (7)']
+    },
+    'biceps brachii': {
+      'origin': ['long head: supraglenoid tubercle of scapula', 'short head: coracoid process of scapula with coracobrachialis'],
+      'insertion': ['posterior border of bicipital tuberosity of radius', 'bicipital aponeurosis to deep fascia and subcutaneous ulna'],
+      'action': ['supinates forearm', 'flexes elbow', 'weakly flexes shoulder'],
+      'nerve': ['musculocutaneous nerve (C5 6)']
+    },
+    'biceps femoris': {
+      'origin': ['long head: upper inner quadrant of posterior surface of ischial tuberosity', 'short head: middle third of linea aspear, lateral supracondylar ridge of femur'],
+      'insertion': ['styloid process of head of fibula', 'lateral collateral ligament', 'lateral tibial condyle'],
+      'action': ['flexes and laterally rotates knee', 'long head: extends hip'],
+      'nerve': ['long head: tibial portion of sciatic nerve', 'short head: common peroneal portion of sciatic nerve (L5 S1)']
+    },
+    'brachialis': {
+      'origin': ['anterior lower half of humerus', 'medial and lateral intermuscular septa'],
+      'insertion': ['coronoid process and tuberosity of ulna'],
+      'action': ['flexes elbow'],
+      'nerve': ['musculocutaneous nerve (C5 6)', 'radial nerve (C7)']
+    },
+    'brachioradialis': {
+      'origin': ['upper two thirds of lateral supracondylar ridge of humerus', 'lateral intermuscular septum'],
+      'insertion': ['base of styloid process of radius'],
+      'action': ['flexes arm at elbow', 'brings forearm into midprone position'],
+      'nerve': ['radial nerve (C5 6)']
+    },
+    'buccinator': {
+      'origin': ['external alveolar margins of maxilla and mandible by molar teeth, to maxillary tubercle and pterygoid hamulus and posterior mylohyoid line respectively, then via pterygomandibular raphe between bones'],
+      'insertion': ['decussates at modioulus of mouth and interdigitates with opposite side'],
+      'action': ['aids mastication', 'tenses cheeks in blowing and whistling', 'aids closure of mouth'],
+      'nerve': ['buccal branch of facial nerve (7)']
+    },
+    'bulbospongiosus': {
+      'origin': ['perineal body', 'midline raphe over corpus spongiosum in male'],
+      'insertion': ['superficial perineal membrane', 'dorsal penile or clitoral aponeurosis'],
+      'action': ['male: aids emptying of urine and ejaculate from urethra', 'female: closes vaginal introitus'],
+      'nerve': ['perineal branch of pudendal nerve (S2 3 4)']
+    }
+  }
+}
